e8c410f6119476b50c0abc318434f0be55b46687
adbarnes <adbarnes@gmail.com>
2023-09-24 17:03:14 +0100

    feat: add preact and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://adbarnes.github.io',
-   // base: '/orbital-orbit',
+  site: 'https://adbarnes.github.io'
+  // base: '/orbital-orbit',
+  ,
+  integrations: [preact()]
 });
